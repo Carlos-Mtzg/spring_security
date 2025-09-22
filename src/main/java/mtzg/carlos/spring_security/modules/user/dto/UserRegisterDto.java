38d495442ac742251b6f0b102b07ed2226cf84ea
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-09-22 14:11:20 -0600

    feat: add validation patterns for email and password in AuthRequest and UserRegisterDto

--- src/main/java/mtzg/carlos/spring_security/modules/user/dto/UserRegisterDto.java
+++ src/main/java/mtzg/carlos/spring_security/modules/user/dto/UserRegisterDto.java
@@ -1,5 +1,9 @@
 package mtzg.carlos.spring_security.modules.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,8 +15,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserRegisterDto {
 
+    public static final String NO_ANGLE_BRACKETS_MESSAGE = "must not contain angle brackets";
+    public static final String NO_ANGLE_BRACKETS_REGEX = "^[^<>]*$";
+
+    @NotBlank(message = "Name is required")
+    @Pattern(regexp = NO_ANGLE_BRACKETS_REGEX, message = NO_ANGLE_BRACKETS_MESSAGE)
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Pattern(regexp = NO_ANGLE_BRACKETS_REGEX, message = NO_ANGLE_BRACKETS_MESSAGE)
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(regexp = NO_ANGLE_BRACKETS_REGEX, message = NO_ANGLE_BRACKETS_MESSAGE)
     private String password;
 
 }
