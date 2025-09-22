f3d2feb0fb12a4f06a1921f3264a1dcea81d668f
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2025-09-22 14:11:31 -0600

    feat: add validation annotations to AuthRequest and UserRegisterDto in AuthController

--- src/main/java/mtzg/carlos/spring_security/auth/AuthController.java
+++ src/main/java/mtzg/carlos/spring_security/auth/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mtzg.carlos.spring_security.modules.user.dto.UserRegisterDto;
 
@@ -17,12 +18,12 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<Object> register(@RequestBody UserRegisterDto request) {
+    public ResponseEntity<Object> register(@RequestBody @Valid UserRegisterDto request) {
         return authService.register(request);
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<Object> authenticate(@RequestBody AuthRequest request) {
+    public ResponseEntity<Object> authenticate(@RequestBody @Valid AuthRequest request) {
         return authService.authentication(request);
     }
 }
